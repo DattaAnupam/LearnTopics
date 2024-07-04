669ea3ca541cf5fb1b90fe15d6124bc795d12360
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-04 20:49:40 +0530

    setup swagger

---
+++ DotNetSolutions/DependencyInjection/DependencyInjection/Service/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace DependencyInjection.Service
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add Swagger service
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(configuration.GetValue<string>("SwaggerApiConfig:version"), new OpenApiInfo
+                {
+                    Title = configuration.GetValue<string>("SwaggerApiConfig:Title") ?? "v1",
+                    Description = configuration.GetValue<string>("SwaggerApiConfig:Description") ?? "Dependecy Injection",
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- DotNetSolutions/DependencyInjection/DependencyInjection/Startup.cs
+++ DotNetSolutions/DependencyInjection/DependencyInjection/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace DependencyInjection
+﻿using DependencyInjection.Service;
+
+namespace DependencyInjection
 {
     public class Startup
     {
@@ -11,16 +13,28 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
 
+            // Register other services
+            services.RegisterServices(Configuration);
+            
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
+            // Enable Swagger UI
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+
+            // Specify endpoint so that can be used as an api
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            } );
+            });
         }
     }
 }
