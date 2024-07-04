d2a5a2c5af7ee00c3b9f47754b6b2c56b43c76a0
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-04 19:49:51 +0530

    use custom startup file

--- DotNetSolutions/DependencyInjection/DependencyInjection/Program.cs
+++ DotNetSolutions/DependencyInjection/DependencyInjection/Program.cs
@@ -1,6 +1,13 @@
+using DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Setup custom start up file
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+startup.Configure(app, app.Environment);
 
 app.Run();
