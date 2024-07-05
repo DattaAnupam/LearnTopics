19801282a5e2ea01386899c13dd465f2d73c112c
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-05 11:29:58 +0530

    initial project setup

---
+++ DotNetSolutions/RazorPages/RazorPages/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace RazorPages
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# LearnTopics
-This repo contains projects on various technologies and languages. It is used to store small topics not specific to a technology or language
+# Feature/razor-pages
+This branch contains implementation of Razor pages
