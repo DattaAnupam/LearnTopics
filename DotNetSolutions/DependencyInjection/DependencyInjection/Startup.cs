13ea01473695c70aeb8c528f7e09856e50d438bf
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-12 15:46:41 +0530

    swagger configuraion

--- DotNetSolutions/DependencyInjection/DependencyInjection/Startup.cs
+++ DotNetSolutions/DependencyInjection/DependencyInjection/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace DependencyInjection
+﻿using Microsoft.OpenApi.Models;
+
+namespace DependencyInjection
 {
     public class Startup
     {
@@ -11,12 +13,31 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
 
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Setup Swagger API",
+                    Description = "Setting up swagger Api for any service",
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
 
+            app.UseMvc();
         }
     }
 }
