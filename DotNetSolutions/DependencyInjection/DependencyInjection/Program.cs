d8f8da7fe4cb6e043aba96ce608d01450857620e
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-15 17:49:09 +0530

    example: 1 of Grouping

--- DotNetSolutions/DependencyInjection/DependencyInjection/Program.cs
+++ DotNetSolutions/DependencyInjection/DependencyInjection/Program.cs
@@ -3,25 +3,15 @@ List<int> integerList = new List<int>()
     1,2, 3, 4, 5, 6, 7, 8, 9, 10,
 };
 
-// Find all the number which are > 5
-// Query Syntax
-var numbers_query = from n in integerList where n > 5 select n;
-foreach (var number in numbers_query)
-{
-    Console.Write(number);
-}
-Console.WriteLine();
-
-
 // Method Syntax
-var numbers_method = integerList.Where(n => n > 5).AsQueryable();
+var numbers_method = integerList.Where(n => n > 5).GroupBy(n => n%2 ==0);
 
-foreach(var number in numbers_method)
+foreach(IGrouping<bool, int> group in numbers_method)
 {
-    Console.Write(number);
+    Console.WriteLine($"{group.Key} : {group.Count()}");
+    foreach(int number in group)
+    {
+        Console.WriteLine(number);
+    }
 }
 
-// Mixed Syntax = Query Syntax + Fluent/Method Syntax
-// Query: Find the some of numbers > 5 in the above list
-var sum = (from n in integerList where n > 5 select n).Sum();
-Console.WriteLine($"\nSum: {sum}");
