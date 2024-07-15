2c1c3081989c11167a2b2625b011de21c7d0c6fe
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-15 16:41:31 +0530

    use of query syntax

--- DotNetSolutions/DependencyInjection/DependencyInjection/Program.cs
+++ DotNetSolutions/DependencyInjection/DependencyInjection/Program.cs
@@ -2,3 +2,13 @@ List<int> integerList = new List<int>()
 {
     1,2, 3, 4, 5, 6, 7, 8, 9, 10,
 };
+
+// Find all the number which are > 5
+
+// Query Syntax
+var numbers = from n in integerList where n > 5 select n;
+
+foreach(var number in numbers)
+{
+    Console.WriteLine(number);
+}
