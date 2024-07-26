ba0456df7102b31b20c252f97df72cd8dccfd7fb
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-27 00:08:08 +0530

    Add About page

--- RazorPages/RazorPages/Pages/About.cshtml.cs
+++ RazorPages/RazorPages/Pages/About.cshtml.cs
@@ -4,8 +4,18 @@ namespace RazorPages.Pages
 {
     public class AboutModel : PageModel
     {
+        public string TimeOfDay { get; set; }
         public void OnGet()
         {
+            TimeOfDay = "evening";
+            if(DateTime.Now.Hour < 18)
+            {
+                TimeOfDay = "afternoon";
+            }
+            if(DateTime.Now.Hour < 12)
+            {
+                TimeOfDay = "morning";
+            }
         }
     }
 }
