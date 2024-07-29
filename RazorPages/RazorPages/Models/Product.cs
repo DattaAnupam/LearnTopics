49997a32f3fde1bf1399e97a5072bf4eee1ed1b1
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-29 20:46:46 +0530

    Update ID Type

--- RazorPages/RazorPages/Models/Product.cs
+++ RazorPages/RazorPages/Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public int ID { get; set; }
+        public Guid ID { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
         public string ImageName { get; set; }
