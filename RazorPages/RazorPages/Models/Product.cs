d11ec48d281aef11d9c201c2b82b458d98e33459
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-07-29 20:46:06 +0530

    Add Product model

---
+++ RazorPages/RazorPages/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPages.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+        public double Pice { get; set; }
+    }
+}
