7773e708075eee86c58e3b5b38ce5d0d86fccf98
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-04 01:57:31 +0530

    create basic CRUD controller for doner

--- BloodDonationApp/BloodDonationApp/Controllers/DonersController.cs
+++ BloodDonationApp/BloodDonationApp/Controllers/DonersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BloodDonationApp.Contexts;
+using BloodDonationApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BloodDonationApp.Controllers
 {
@@ -6,10 +9,53 @@ namespace BloodDonationApp.Controllers
     [ApiController]
     public class DonersController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Index()
+        #region Method Variables
+        private readonly BloodDonationDBContext _bdContext;
+        #endregion
+
+        public DonersController(BloodDonationDBContext bdContext)
         {
-            return Ok(new { Message = "Success" });
+            _bdContext = bdContext;
+        }
+
+        // Get all the Doners
+        [HttpGet]
+        [Route("GetDoners")]
+        public async Task<IEnumerable<Doners>> GetDoners()
+        {
+            return await _bdContext.Doners.ToListAsync();
+        }
+
+        // Get Doner by ID
+        [HttpGet]
+        [Route("GetDoner")]
+        public async Task<Doners?> GetDoner([FromQuery] int id)
+        {
+            Doners? doner = await _bdContext.Doners.FindAsync(id);
+            return doner;
+        }
+
+        // Add new Doner
+        [HttpPost]
+        [Route("CreateDoner")]
+        public async Task<string> CreateDoner([FromBody] Doners request)
+        {
+            await _bdContext.Doners.AddAsync(request);
+            await _bdContext.SaveChangesAsync();
+
+            return "done";
+        }
+
+        // Delete Doner By ID
+        [HttpDelete]
+        [Route("DeleteDoner")]
+        public async Task<string> DeleteDoner([FromQuery] int id)
+        {
+            var donerToDelete = await _bdContext.Doners.FindAsync(id);
+            _bdContext.Doners.Remove(donerToDelete);
+            await _bdContext.SaveChangesAsync();
+
+            return "Deleted";
         }
     }
 }
