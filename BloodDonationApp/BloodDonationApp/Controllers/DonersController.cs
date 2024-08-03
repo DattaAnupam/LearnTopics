2f6337cceba65d97534a00de546679b3e451af80
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-04 01:15:41 +0530

    Add doner controller - basic

---
+++ BloodDonationApp/BloodDonationApp/Controllers/DonersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonationApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { Message = "Success" });
+        }
+    }
+}
