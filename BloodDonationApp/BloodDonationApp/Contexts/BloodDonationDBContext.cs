ec69b54723250fc610ae4b3a9bc0dc8b57d2a466
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-03 22:18:47 +0530

    add basic db context class

---
+++ BloodDonationApp/BloodDonationApp/Contexts/BloodDonationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationApp.Contexts
+{
+    public class BloodDonationDBContext : DbContext
+    {
+        public BloodDonationDBContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
