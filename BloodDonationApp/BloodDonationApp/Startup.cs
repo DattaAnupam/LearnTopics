1d2617e0c60939da105c09e4030ab54b81ffc1fe
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-03 22:31:52 +0530

    add Startup.cs file

---
+++ BloodDonationApp/BloodDonationApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace BloodDonationApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
