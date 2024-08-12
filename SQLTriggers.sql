930dfe31701e973581c1154498fc52d6d42660f0
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-13 00:18:03 +0530

    clean branch

--- SQLTriggers.sql
+++
@@ -1,5 +0,0 @@
--- Triggers
--- =============
--- A trigger is a database object that runs automatically when an event occurs. 
--- DML Triggers
--- DML Triggers are fired when a DML event occures. i.e. when data is inserted/update/delete etc
