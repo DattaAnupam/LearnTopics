69f5c2b59434d0b2bbf6e086b3839b7985c6609f
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-12 23:13:27 +0530

    delegate with returning value

--- Deligates/Deligates/Deligate.cs
+++ Deligates/Deligates/Deligate.cs
@@ -4,5 +4,7 @@
     {
         public static void MethodA(string message) => Console.WriteLine(message);
         public static void MethodB(MyDelegate del) => del("Delegate passed as parameter");
+
+        public static int MethodC(int num1, int num2) => num1 + num2;
     }
 }
--- Deligates/Deligates/Program.cs
+++ Deligates/Deligates/Program.cs
@@ -2,12 +2,17 @@
 {
     // Diclare delegate
     public delegate void MyDelegate(string msg);
+    public delegate int MyDelegate2(int num1, int num2);
     public class Program
     {
         public static void Main(string[] args)
         {
             MyDelegate delA = ClassA.MethodA;
             delA("Calling MethodA() using Deligate");
+
+            // Delegate with returning value
+            MyDelegate2 delB = ClassA.MethodC;
+            Console.WriteLine($"Sum of two number using delegate: {delB(3, 4)}");
 
             // Deligate with Lamda function
             // Assigning lamda function to the delegate
@@ -16,6 +21,10 @@
 
             // Pass deligate as argument
             ClassA.MethodB(delA);
+
+            #region Multi cast Delegate
+            
+            #endregion
         }
     }
 }
