d4b6896cc888893f48453854184940cbc5190e49
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-13 00:47:47 +0530

    add Func with lamda function

--- Deligates/FuncDelegates/Program.cs
+++ Deligates/FuncDelegates/Program.cs
@@ -7,3 +7,7 @@ Func<int, int, int> add = Sum;
 
 // Use Func
 Console.WriteLine($"Sum of two numbers: {add(4, 5)}");
+
+// Function with Lamda function
+Func<string, string> greet = (string userName) => $"Hello {userName}!!!";
+Console.WriteLine(greet("Anupam"));
