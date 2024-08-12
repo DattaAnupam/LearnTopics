f968a6f005d04687d9a34066e42535d6523683f8
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-13 00:10:13 +0530

    code restructuring

--- Deligates/Deligates/Program.cs
+++ Deligates/Deligates/Program.cs
@@ -1,27 +1,34 @@
 ﻿namespace Deligates
 {
-    // Diclare delegate
+    #region Declare delegates
     public delegate void MyDelegate(string msg);
     public delegate int MyDelegate2(int num1, int num2);
     public delegate void MyDelegate3<T>(T obj);
+    #endregion
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            #region Simple delegate
             MyDelegate delA = ClassA.MethodA;
             delA("Calling MethodA() using Deligate");
+            #endregion
 
-            // Delegate with returning value
+            #region Delegate with returning value
             MyDelegate2 delB = ClassA.MethodC;
             Console.WriteLine($"Sum of two number using delegate: {delB(3, 4)}");
+            #endregion
 
-            // Deligate with Lamda function
+            #region Deligate with Lamda function
             // Assigning lamda function to the delegate
             delA = (string msg) => { Console.WriteLine(msg); };
             delA("This delegate holds reference to the lamda function");
+            #endregion
 
-            // Pass deligate as argument
+            #region Pass deligate as argument
             ClassA.MethodB(delA);
+            #endregion
 
             #region Multi cast Delegate
             MyDelegate delC = ClassA.MethodA;
