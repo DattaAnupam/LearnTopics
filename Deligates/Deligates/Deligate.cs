437141f7ebdd0ca553ba9d0064aa402b7e6804d9
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-12 22:51:12 +0530

    basic deligate

---
+++ Deligates/Deligates/Deligate.cs
@@ -0,0 +1,7 @@
+﻿namespace Deligates
+{
+    public class ClassA
+    {
+        public static void MethodA(string message) => Console.WriteLine(message);
+    }
+}
--- Deligates/Deligates/Program.cs
+++ Deligates/Deligates/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Deligates
+{
+    // Diclare delegate
+    public delegate void MyDelegate(string msg);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyDelegate delA = ClassA.MethodA;
+            delA("Calling MethodA() using Deligate");
+        }
+    }
+}
