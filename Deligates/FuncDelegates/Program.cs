3c867c54913e860ce030878e1b6f4d9063381f80
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-13 00:43:17 +0530

    add simple Func example

--- Deligates/FuncDelegates/Program.cs
+++ Deligates/FuncDelegates/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+static int Sum(int num1, int num2) => num1 + num2;
+
+// Declare Func with two parameters
+Func<int, int, int> add = Sum;
+
+// Use Func
+Console.WriteLine($"Sum of two numbers: {add(4, 5)}");
