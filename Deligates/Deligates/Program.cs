b6640b4e3d210e24e798aa726f3c45f554133952
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-12 22:54:06 +0530

    delegate with lamda function

--- Deligates/Deligates/Program.cs
+++ Deligates/Deligates/Program.cs
@@ -8,6 +8,11 @@
         {
             MyDelegate delA = ClassA.MethodA;
             delA("Calling MethodA() using Deligate");
+
+            // Deligate with Lamda function
+            // Assigning lamda function to the delegate
+            delA = (string msg) => { Console.WriteLine(msg); };
+            delA("This delegate holds reference to the lamda function");
         }
     }
 }
