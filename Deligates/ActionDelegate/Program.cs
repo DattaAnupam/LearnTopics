44aede5eb86cf3a43854d9181892ae855dcc8183
DattaAnupam <aanupamdatta.1995@gmail.com>
2024-08-13 01:11:16 +0530

    Action with Lamda expression

--- Deligates/ActionDelegate/Program.cs
+++ Deligates/ActionDelegate/Program.cs
@@ -1,8 +1,18 @@
 ﻿static void Greet(string name) => Console.WriteLine($"Hello {name}!!!");
 
-#region Simple Func delegate
+#region Simple Action delegate
 Action<string> greetAction = Greet;
 
 greetAction("Anupam");
 #endregion
 
+#region Action with Lamda Function
+Action<int, int> PrintTwoNumber = (int num1, int num2) =>
+{
+    Console.WriteLine($"Num1: {num1}");
+    Console.WriteLine($"Num1: {num2}");
+};
+
+Console.WriteLine("Action with Lamda expression");
+PrintTwoNumber(45, 30);
+#endregion
